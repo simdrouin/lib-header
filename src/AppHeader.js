a7fee126916b7a8e53f379a4c216d46ffadd7ac4
Simon Drouin <simdrouin@gmail.com>
2018-12-21 14:43:54 -0500

    Initial version without Redux

---
+++ src/AppHeader.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AppHeader extends Component {
+    render() {
+        return <div>Hello from library</div>;
+    }
+}
+
+export default AppHeader;
